912d7d9204e7267951be75dafafa452c98911a8d
allwen <ArianGhasempour40@Gmail.com>
2016-06-22 14:34:08 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,15 +1,16 @@
-local function run(msg, matches)
-return "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id.."\n"
-
+do
+function run(msg, matches)
+local reply_id = msg['id']
+local info = "~Your Information & Id ⬇\n|Id|: "..msg.from.id.."\n|Name|: "..msg.from.print_name.."\n|Phone|: "..msg.from.phone.."\n|Msg|: "..msg.text.."\n~Group Information & Id ⬇\n|Id|: "..msg.to.id.."\n|Name|: "..msg.to.print_name.."\n"
+reply_msg(reply_id, info, ok_cb, false)
 end
 
 return {
 patterns = {
-"^[!/#](id)$",
-"^([Ii]d)$"
+"^[!/#]([Ii][Dd])$",
+"^([Ii]d)"
 },
 run = run
 }
 
-    end
 end
