7f93a8f8477e498821a98fd107fb988e422fb5c6
allwen <ArianGhasempour40@Gmail.com>
2016-06-22 14:23:24 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,6 +1,8 @@
 local function run(msg, matches)
 return "~Your #Name: "..msg.from.print_name.."\n~Your #Username: @"..msg.from.username.."\n~Your #ID: "..msg.from.id.."\n~Your #Message: "..msg.text.."\n~Your #Phone: +"..msg.from.phone.."\n~SuperGroup #Name: "..msg.to.print_name.."\n~SuperGroup #Id: "..msg.to.id.."\n"
 
+end
+
 return {
 patterns = {
 "^[!/#](id)$",
